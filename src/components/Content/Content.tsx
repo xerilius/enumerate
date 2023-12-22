5d49e8a6bac393c47c4d25a30efa636819b41235
Grace <gcw.chung@gmail.com>
2023-12-22 03:57:46 -0800

    Add Content component

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Form } from "../Form";
+
+import styles from "./Content.module.scss";
+
+export const Content = () => {
+  return (
+    <div className={styles.Content}>
+      <Form />
+    </div>
+  );
+};
