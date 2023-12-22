a1f4426874102bf517207cdfff7ef3617876c0dd
Grace <gcw.chung@gmail.com>
2023-12-22 04:01:05 -0800

    Add Navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./Navbar.module.scss";
+
+export const Navbar = () => {
+  return (
+    <div className={styles.Navbar}>
+      <div className={styles.Logo}>enum.</div>
+    </div>
+  );
+};
